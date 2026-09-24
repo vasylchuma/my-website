d6892ee12e864d5ffc3f53a557af5ef0f75a06a5
vasylchuma <vasylchuk.m@nemk.ukr.education>
2026-09-24 22:24:18 +0300

    da

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <base href="<?php echo get_template_directory_uri(); ?>/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ШІ підказки</title>
