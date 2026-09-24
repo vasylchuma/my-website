1948080c5f0b93a331014994ff7f36f5f7b5dc4b
vasylchuma <vasylchuk.m@nemk.ukr.education>
2026-09-24 22:04:59 +0300

    памагітє

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ШІ підказки</title>
     <link rel="stylesheet" href="Vasylchuk_style.css">
+    <link rel="stylesheet" href="adapt.css">
 </head>
 <body>
 
